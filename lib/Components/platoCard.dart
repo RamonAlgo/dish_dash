9e22a4e73c9324818307214e2a53988b7b3a7772
pau <pauserra@ceroca.cat>
2024-03-14 17:55:13 +0100

    Correccions

--- lib/Components/platoCard.dart
+++ lib/Components/platoCard.dart
@@ -35,12 +35,13 @@ class PlatoCard extends StatelessWidget {
           Expanded(
             child: Image.asset(
               plato.imageUrl,
+              width: double.infinity,
               fit: BoxFit.cover,
             ),
           ),
           if (plato.descripcion.isNotEmpty)
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Text(
                 plato.descripcion,
                 style: const TextStyle(color: Colors.grey),
@@ -48,17 +49,69 @@ class PlatoCard extends StatelessWidget {
             ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text('Precio: \$${plato.precio.toString()}'),
+            child: Text('Precio: \€${plato.precio.toString()}'),
           ),
-          ElevatedButton(
-            onPressed: onAdd,
-            style: ElevatedButton.styleFrom(
-   
-            ),
-            child: const Text('Demanar'),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () => _mostrarDialogoIngredientes(context),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.green,
+                ),
+                child: const Text('Info'),
+              ),
+              ElevatedButton(
+                onPressed: onAdd,
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.green,
+                ),
+                child: const Text('Demanar'),
+              ),
+            ],
           ),
         ],
       ),
+    );
+  }
+
+  void _mostrarDialogoIngredientes(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.white.withOpacity(0.85),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            constraints: BoxConstraints(maxHeight: 300),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  plato.nombrePlato,
+                  style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green),
+                ),
+                SizedBox(height: 10),
+                const Text(
+                  "Ingredientes:",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                ...plato.ingredientes
+                    .map((ingrediente) => Text(ingrediente))
+                    .toList(),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/pagines/menus/pagina_menu_client.dart
+++ lib/pagines/menus/pagina_menu_client.dart
@@ -1,9 +1,11 @@
-import 'package:dish_dash/Clases/Plat.dart';
+import 'package:dish_dash/Clases/model_dades.dart';
 import 'package:dish_dash/Components/menuCard.dart';
 import 'package:flutter/material.dart';
+import 'package:dish_dash/Clases/Plat.dart';
+import 'package:provider/provider.dart';
 
 class PaginaMenuClient extends StatelessWidget {
-  const PaginaMenuClient({super.key});
+  const PaginaMenuClient({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -47,19 +49,23 @@ class PaginaMenuClient extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.all(8.0),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
+          crossAxisCount: 3, 
           crossAxisSpacing: 8.0,
           mainAxisSpacing: 8.0,
         ),
         itemCount: platos.length,
         itemBuilder: (context, index) {
           final plato = platos[index];
-          return menuCard(
-            imageUrl: plato.imageUrl,
-            nombrePlato: plato.nombrePlato,
-            descripcion: plato.descripcion,
-            ingredientes: plato.ingredientes,
-            precio: plato.precio,
+          return MenuCard(
+            plato: plato,
+            onAdd: () {
+              Provider.of<ModelDades>(context, listen: false)
+                  .agregarAlCarrito(plato);
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                    content: Text('${plato.nombrePlato} añadido al carrito')),
+              );
+            },
           );
         },
       ),
