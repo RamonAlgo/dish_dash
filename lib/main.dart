84f081ff2cb74f4e715e873509e9874a8dca886e
Ramon <ramonandres@ceroca.cat>
2024-05-13 18:50:41 +0200

    US18
    Echo:
    Recolección de los 6 platos mas pedidos del mes
    
    Por hacer:
    Mostrar los 6 platos con un componente "PlatoCard"

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:dish_dash/Clases/model_dades.dart';
 import 'package:dish_dash/firebase_options.dart';
 import 'package:dish_dash/pagina_login.dart';
-import 'package:dish_dash/pagines/administrador/adminDashboard.dart';
-import 'package:dish_dash/pagines/administrador/administrar_Menu.dart';
-import 'package:dish_dash/pagines/administrador/pagina_administrador.dart';
-import 'package:dish_dash/pagines/landing/landingpage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
