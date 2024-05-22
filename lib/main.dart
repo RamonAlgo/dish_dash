f265c4947ae6130b785f5ca78d2692a52fcc540d
Ramon <ramonandres@ceroca.cat>
2024-05-22 17:10:31 +0200

    import landing

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:dish_dash/pagina_login.dart';
 import 'package:dish_dash/pagines/administrador/adminDashboard.dart';
 import 'package:dish_dash/pagines/administrador/administrar_Menu.dart';
 import 'package:dish_dash/pagines/cuina/pagina_cuina.dart';
+import 'package:dish_dash/pagines/landing/landingpage.dart';
 import 'package:dish_dash/pagines/pagina_inicial_client.dart';
 import 'package:dish_dash/tpv/paginatpv.dart';
 import 'package:firebase_core/firebase_core.dart';
